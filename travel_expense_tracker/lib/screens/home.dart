779f64248cd430292c7ebcb2df48fe3182e04acf
chloe <cschmi29@wgu.edu>
2026-03-01 16:48:30 -0600

    creates base for home screen

---
+++ travel_expense_tracker/lib/screens/home.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/trip_provider.dart';
+import 'package:intl/intl.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final tripProvider = context.watch<TripProvider>();
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Trips')),
+      body: Center(
+        child: ListView.builder(
+          itemCount: tripProvider.trips.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(tripProvider.trips[index].name),
+              subtitle: Text(
+                '${tripProvider.trips[index].destination} - ${DateFormat('MMM d, y').format(tripProvider.trips[index].startDate)}',
+              ),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                // Navigate to trip details
+              },
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigate to add trip screen
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
